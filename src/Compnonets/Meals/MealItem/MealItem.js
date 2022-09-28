563bb338083ebfddff02b0ed444748564ccdc704
georgeowusu <gkojunior@gmail.com>
2022-09-28 10:48:15 -0400

    Added individual meal items and displayed them

---
+++ src/Compnonets/Meals/MealItem/MealItem.js
@@ -0,0 +1,16 @@
+import classes from './MealItem.module.css';
+
+export default function MealItem({name, description, price }) {
+	const prices = `$${price.toFixed(2)}`;
+
+	return (
+	<li className={classes.meal}>
+		<div>
+			<h3>{name}</h3>
+			<div className={classes.description}>{description}</div>
+			<div className={classes.price}>{prices}</div>
+		</div>
+		<div></div>
+        </li>
+    )    
+}
